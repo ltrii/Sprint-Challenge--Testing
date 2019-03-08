b3ab9d729f413e0e9e487f6567e022e9be27d013
ltrii <lelandtr@gmail.com>
2019-03-08 08:45:54 -0800

    API works

---
+++ data/migrations/20190308083618_games.js
@@ -0,0 +1,14 @@
+
+exports.up = function(knex, Promise) {
+  return knex.schema.createTable('games', tbl => {
+      tbl.increments();
+
+      tbl.string("title").notNullable();
+      tbl.string("genre").notNullable();
+      tbl.integer("releaseYear").notNull().defaultTo(1980);
+  })
+};
+
+exports.down = function(knex, Promise) {
+  return knex.schema.dropTableIfExists('games');
+};
