fb266891b394de1fc270105b91f7cd893a7537db
ltrii <lelandtr@gmail.com>
2019-03-08 08:34:34 -0800

    Init API work

---
+++ api/server.js
@@ -0,0 +1,34 @@
+const express = require('express');
+
+const games = require('../games/gamesModel.js');
+
+const server = express();
+
+server.use(express.json());
+
+server.get('/', async (req, res) => {
+  res.status(200).json({ api: 'Running...' });
+});
+
+server.get('/games', async (req, res) => {
+  const rows = await games.getAll();
+
+  res.status(200).json(rows);
+});
+
+server.post('/games', async (req, res) => {
+    const game = req.body;
+    if(game.homeScore && game.awayScore){
+        games.insert(game)
+            .then(newgame => {
+                res.status(201).json(newgame);
+            })
+            .catch(() => {
+                res.status(500).json({ message: 'game failed to add'})
+            })
+    } else {
+        res.status(400).json({ message: 'Missing game details' })
+    }
+})
+
+module.exports = server;
